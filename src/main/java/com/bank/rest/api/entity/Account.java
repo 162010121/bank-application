41245edc2daa25b603ab7ae46a9c4747c4f0e575
Praveen <Praveensonu1999@gmail.com>
2024-07-04 12:49:28 +0530

    Add Some Functinality

--- src/main/java/com/bank/rest/api/entity/Account.java
+++ src/main/java/com/bank/rest/api/entity/Account.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -40,12 +38,10 @@ public class Account {
 	@Column(name = "AccountNumber")
 	private Long accountNumber;
 
-	
-
 	@Column(name = "AccountType")
 	private String accountType;
-	
-	@Column(name="AccountStatus")
+
+	@Column(name = "AccountStatus")
 	private String accountStatus;
 
 }
--- src/main/java/com/bank/rest/api/exception/CustomeCustomerException.java
+++ src/main/java/com/bank/rest/api/exception/CustomeCustomerException.java
@@ -6,11 +6,10 @@ public class CustomeCustomerException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	public CustomeCustomerException (String message)
-	{
+
+	public CustomeCustomerException(String message) {
 		super(message);
-	
+
 	}
 
 }
